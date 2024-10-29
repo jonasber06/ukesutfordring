37a94c70e312feab7a8ee88071139c9169de4076
jonasber06 <144693393+jonasber06@users.noreply.github.com>
2024-10-29 09:40:06 +0100

    oppdatering

--- uke43.py
+++ uke43.py
@@ -1,4 +1,4 @@
-
+print("")
 n = 1000
 
 def f(x):
@@ -12,13 +12,13 @@ s = 0
 
 for i in range(n):
     xi = a+dx*i
-    s+=(f(xi)*dx)
+    s+=(f(xi))
 print(s)
 
 s1 = 0
-for i in range(n+1):
-    xi = a+dx*i
-    s1+=(f(xi)*dx)
+for n in range(n+1):
+    xi = a+dx*n
+    s1+=f(xi)*dx
 
 integral = (dx/2)*(f(a)+f(b)+2*s1)
 print(integral)
