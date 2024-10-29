3bcee1a9f21d424b82f981f82e14f387f41453ec
jonasber06 <144693393+jonasber06@users.noreply.github.com>
2024-10-29 09:42:29 +0100

    midtpunktmetode og trapesmetode

--- uke43.py
+++ uke43.py
@@ -11,14 +11,14 @@ dx = (b-a)/n
 s = 0
 
 for i in range(n):
-    xi = a+dx*i
-    s+=(f(xi))
+    xi = a+dx*(i-(1/2))
+    s+=f(xi)*dx
 print(s)
 
 s1 = 0
 for n in range(n+1):
     xi = a+dx*n
-    s1+=f(xi)*dx
+    s1+=f(xi)
 
 integral = (dx/2)*(f(a)+f(b)+2*s1)
 print(integral)
